bebc70acd38e83a9340b385ffa69bc97448df041
OBenjOne <bschoolland@gmail.com>
2023-03-16 00:58:13 -0700

    third commit

---
+++ app.js
@@ -0,0 +1,33 @@
+
+//Require the neccesary libraries and modules 
+// set up express
+const express = require("express");
+const app = express();
+app.use(express.static("public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// allow the client to transfer data
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const multer = require("multer");
+const upload = multer({ dest: "uploads/" });
+
+
+const display_all_applications = require("./display-files/display-all-applications");
+
+
+
+// send user to index page when they search our website url
+app.get("/", function (req, res) {
+  display_all_applications(req, res);  
+});
+
+
+// Server configuration
+const port = process.env.PORT || 3000;
+
+app.listen(port, function(){
+  console.log(`Server is running on port ${port}`);
+});
